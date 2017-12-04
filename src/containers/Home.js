83bbecf16a491e752fc26f7b747488ae727efbce
Thomas Adams <tjackadams@itadams.co.uk>
2017-12-04 15:29:42 +0000

    Migrated over to react-static. Created the layout and menu structure.

---
+++ src/containers/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { getSiteProps } from 'react-static'
+//
+
+export default getSiteProps(() => (
+  <div>
+    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+  </div>
+))
